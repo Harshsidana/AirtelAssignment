ddfdc4a9325e6802345fd4b7d8374a75b1544ffc
harshsidana <harsh.sidana@appstreet.io>
2019-08-04 18:58:25 +0530

    HomeScreen Done

--- src/main/java/com/appstreet/fitness/ui/home/HomeActivity.kt
+++ src/main/java/com/appstreet/fitness/ui/home/HomeActivity.kt
@@ -2,10 +2,17 @@ package com.appstreet.fitness.ui.home
 
 import android.os.Bundle
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.appstreet.base.cell.Cell
+import com.appstreet.base.extension.showLongToast
 import com.appstreet.base.model.DataState
 import com.appstreet.fitness.R
+import com.appstreet.fitness.ui.home.adapter.Adapter
+import com.appstreet.home.cell.HomeCell
 import com.appstreet.home.model.response.ApiReponse
 import com.appstreet.home.view.AbstractHomeActivity
+import kotlinx.android.synthetic.main.activity_home.*
 
 
 class HomeActivity : AbstractHomeActivity() {
@@ -20,9 +27,19 @@ class HomeActivity : AbstractHomeActivity() {
         viewModel.getCardCellDetails().observe(this, Observer<DataState<List<ApiReponse>>> {
             when (it) {
                 is DataState.Success -> {
+                    val list: MutableList<Cell> = ArrayList()
+                    for (i in 0 until it.data.size) {
+                        list.add(HomeCell(it.data[i]))
+                    }
+
+                    val rvAdapter = Adapter(list)
+                    recyclerView.layoutManager = LinearLayoutManager(applicationContext, RecyclerView.VERTICAL, false)
+                    recyclerView.adapter = rvAdapter
+
+
                 }
                 is DataState.Failure -> {
-
+                    showLongToast("Some Error fetching daya")
                 }
             }
 
